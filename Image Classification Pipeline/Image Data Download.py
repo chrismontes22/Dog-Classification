7e1ca65db93d8f19af43811b32469116c12aba8d
chrismontes22 <chrismontes22@outlook.com>
2024-06-11 17:15:34 -0700

    Added logging to the image data pipeline. Also annotated the code for more information

--- Image Classification Pipeline/Image Data Download.py
+++ Image Classification Pipeline/Image Data Download.py
@@ -1,58 +1,82 @@
-#Image Data Download
+"""The script requires you to create your own google account, then go https://developers.google.com/custom-search/v1/overview to create your own custom image search.
+once the image search is ccreated you will receive a cse_id and you will be able to create an API key. If you want to hide the ID and key you will also require
+a seperate config file, such as in the script below"""
 
 import os
 import requests
 from PIL import Image
 from io import BytesIO
+import logging
+import datetime
 import config
 
-#Parameters
-search_query = 'cat' #Google Search query
-folder_path = search_query.replace(' ', '-')
-first_image = 150
-last_image = 159 #Google allows max of 100 per run, It will fininsh the last page (set of 10 images)
+#Set up logging
+logging.basicConfig(filename='Image Changes.txt', level=logging.INFO, format='%(message)s')
 
+#Parameters
+search_query = 'puppy' #Google Search query
+folder_path = search_query.replace(' ', '-')
+first_image = 11 #Google search api requres you set a range of images. The max difference between the two is 100 images
+last_image = 1100 #Google allows max of 100 per run, It will fininsh the last page (set of 10 images)
+                  #Start where you left off last run. The code loggeed this number in a text file in the main workfolder
+                  
+
+# Log the dog breeds and the current time
+logging.info('Image Download from Google')
+logging.info('Time: ' + str(datetime.datetime.now()))
+logging.info('Search Query: ' + search_query)
+logging.info('First Image: ' + str(first_image))
+logging.info('Last Image: ' + str(last_image))
+logging.info('\n')
 
 # Set up your API key and Custom Search Engine ID
 api_key = config.google_api_key
 cse_id = config.google_cse_id
 
-
-def google_image_search(query, api_key, cse_id, start_index, num_images=10):
-    search_url = "https://www.googleapis.com/customsearch/v1"
-    params = {
-        'q': query,
-        'key': api_key,
-        'cx': cse_id,
-        'searchType': 'image',
-        'start': start_index,
-        'num': num_images
-    }
-    response = requests.get(search_url, params=params)
-    response.raise_for_status()
-    return response.json()
-
-def download_image(url, folder_path, image_name, timeout=10):
-    try:
-        response = requests.get(url, timeout=timeout)
+try:
+    def google_image_search(query, api_key, cse_id, start_index, num_images=10):
+        search_url = "https://www.googleapis.com/customsearch/v1"
+        params = {
+            'q': query,
+            'key': api_key,
+            'cx': cse_id,
+            'searchType': 'image',
+            'start': start_index,
+            'num': num_images
+        }
+        response = requests.get(search_url, params=params)
         response.raise_for_status()
-        image = Image.open(BytesIO(response.content))
-        image_path = os.path.join(folder_path, image_name)
-        image.save(image_path)
-    except requests.exceptions.Timeout:
-        print(f"Request for {url} timed out.")
-    except requests.exceptions.RequestException as e:
-        print(f"Could not download {url} - {e}")
-    except Exception as e:
-        print(f"Could not save {url} - {e}")
+        return response.json()
 
-if not os.path.exists(folder_path):
-    os.makedirs(folder_path)
+    def download_image(url, folder_path, image_name, timeout=10):
+        #downloads an image from a URL, checks the response status, opens the image, and saves it to a specified folder with a given name.
+        try:
+            response = requests.get(url, timeout=timeout)
+            response.raise_for_status()
+            image = Image.open(BytesIO(response.content))
+            image_path = os.path.join(folder_path, image_name)
+            image.save(image_path)
+        except requests.exceptions.Timeout:
+            print(f"Request for {url} timed out.")
+        except requests.exceptions.RequestException as e:
+            print(f"Could not download {url} - {e}")
+        except Exception as e:
+            print(f"Could not save {url} - {e}")
 
-for start_index in range(first_image, last_image + 1, 10):
-    search_results = google_image_search(search_query, api_key, cse_id, start_index, num_images=10)
-    for i, item in enumerate(search_results.get('items', [])):
-        img_url = item['link']
-        image_index = start_index + i
-        download_image(img_url, folder_path, f'image_{image_index}.jpg')
+  # Creates a folder for the new images of the new breed
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+
+#A loop that establishes the range of images to downlaod (form above parameters). 10 is the max number of images per request, and it loops to the next ten each request
+#Calculates an index for the current image based on the start index and the current index.
+    for start_index in range(first_image, last_image + 1, 10):
+        search_results = google_image_search(search_query, api_key, cse_id, start_index, num_images=10)
+        for i, item in enumerate(search_results.get('items', [])):
+            img_url = item['link']
+            image_index = start_index + i
+            download_image(img_url, folder_path, f'image_{image_index}.jpg')
+except Exception as e:
+    logging.error('Error occurred: {}'.format(e))
+    logging.info('\n')
+print("Images downloaded successfully!")
 
--- Image Classification Pipeline/Organizing image data and zipping it.py
+++ Image Classification Pipeline/Organizing image data and zipping it.py
@@ -1,47 +1,83 @@
-####upload pipeline
+"""After you have downloaded the image data for the selected breeds, this script renames the images in a padded 3 digit number in order, then creates test 
+train val split folders in the main image folder. Next it moves the appropriate amount of images to the corresponding folders.
+Finally it zips the entire image dataset"""
+"""Please note that the dog_breeds here are filepaths, so if the folders are saved in a different path you must change it in the
+dog_breeds list accordingly"""
+
 import os
 import shutil
 import random
+import logging
+import datetime
+
+# Set up logging
+logging.basicConfig(filename='Image Changes.txt', level=logging.INFO, format='%(message)s')
+
+# List of dog breeds
+dog_breeds = ['Akita', 'Alaskan-Malamute', 'American-Foxhound', 'Cane-Corso', 'Dachshund', 'German-Shorthaired-Pointer', 'Miniature-Schnauzer', 'Stafforshire-Bull-Terrier' ]  # Add all dog breeds to this list
+
+# Define the path to the large folder
+large_folder = 'DD'
+
+# Log the dog breeds and the current time
+logging.info('***********************Zip File Change***********************')
+logging.info('Time: ' + str(datetime.datetime.now()))
+logging.info('Dog Breeds: ' + str(dog_breeds))
 
 
-# Define the paths to your folders
-new_images_folder = 'Akita - Copy'
-large_folder = 'DC6 - Copy'
+try:
+    for dog_breed in dog_breeds:
+        # Set the starting number
+        num = 1
 
-# Get the list of all image files in the new_images_folder
-image_files = [f for f in os.listdir(new_images_folder) if os.path.isfile(os.path.join(new_images_folder, f))]
+        # Sometimes image filenames are all over the place. This renames the files in numerical order in a folder. Loops for each breed
+        for filename in os.listdir(dog_breed):
+            # Check if the file is an image
+            if filename.endswith(".jpg") or filename.endswith(".png") or filename.endswith(".jpeg"):
+                # Construct the new filename with the numerical name
+                new_filename = "{:03d}.{}".format(num, filename.split(".")[-1])
+                num += 1
+                # Rename the file
+                os.rename(os.path.join(dog_breed, filename), os.path.join(dog_breed, new_filename))
 
-# Shuffle the list to ensure randomness
-random.shuffle(image_files)
+        # Get the list of all image files in the dog_breed
+        image_files = [f for f in os.listdir(dog_breed) if os.path.isfile(os.path.join(dog_breed, f))]
 
-# Split the data into test, valid, and train
-test_files = image_files[:10]
-valid_files = image_files[10:20]
-train_files = image_files[20:]
+        # Shuffle the list to ensure randomness
+        random.shuffle(image_files)
 
-# Define the new label name
-new_label_name = os.path.basename(new_images_folder)
+        # Split the data into test, valid, and train
+        test_files = image_files[:10]
+        valid_files = image_files[10:20]
+        train_files = image_files[20:]
 
-# Create new label folders inside test, train, and valid folders
-os.makedirs(os.path.join(large_folder, 'test', new_label_name), exist_ok=True)
-os.makedirs(os.path.join(large_folder, 'valid', new_label_name), exist_ok=True)
-os.makedirs(os.path.join(large_folder, 'train', new_label_name), exist_ok=True)
+        # Define the new label name
+        new_label_name = os.path.basename(dog_breed)
 
-# Function to move files
-def move_files(files, dst_folder):
-    for file in files:
-        shutil.move(os.path.join(new_images_folder, file), os.path.join(dst_folder, file))
+        # Create new label folders inside test, train, and valid folders
+        os.makedirs(os.path.join(large_folder, 'test', new_label_name), exist_ok=True)
+        os.makedirs(os.path.join(large_folder, 'valid', new_label_name), exist_ok=True)
+        os.makedirs(os.path.join(large_folder, 'train', new_label_name), exist_ok=True)
 
-# Move the files to the appropriate folders
-move_files(test_files, os.path.join(large_folder, 'test', new_label_name))
-move_files(valid_files, os.path.join(large_folder, 'valid', new_label_name))
-move_files(train_files, os.path.join(large_folder, 'train', new_label_name))
+        # Function to move files
+        def move_files(files, dst_folder):
+            for file in files:
+                shutil.move(os.path.join(dog_breed, file), os.path.join(dst_folder, file))
 
+        # Move the files to the appropriate folders
+        move_files(test_files, os.path.join(large_folder, 'test', new_label_name))
+        move_files(valid_files, os.path.join(large_folder, 'valid', new_label_name))
+        move_files(train_files, os.path.join(large_folder, 'train', new_label_name))
+#Useful for logging an error occurred and test train split folders were not able to be created
+except Exception as e:
+    logging.error('Error occurred: {}'.format(e))
+    raise
 
-import shutil
+#Makes a zip copy from the above data saved to large_folder variable
 def zip_folder(folder_path, zip_name):
-    # Create a Zip file
     shutil.make_archive(zip_name, 'zip', folder_path)
+    # Log the zip file name
+    logging.info('Zip file name: ' + zip_name + '.zip')
+    logging.info('\n')
 # Usage
-zip_folder(large_folder, 'DC7')
-
+zip_folder(large_folder, 'DD3')
